8da7ad23b9da8c57a014b4cec4efbc641ce5784e
Lorenzo Masini <rugginoso@develer.com>
2012-07-21 23:28:13 +0200

    Added limit parameter to search query.

--- moodeque/stereomood.py
+++ moodeque/stereomood.py
@@ -10,7 +10,7 @@ from moodeque.models import Song
 
 class StereoMoodClient(object):
   POOR_AUTH_URL = "http://www.stereomood.com/api/hackitaly/auth.php"
-  SEARCH_URL = "http://www.stereomood.com/api/search.json?q=%s&type=%s"
+  SEARCH_URL = "http://www.stereomood.com/api/search.json?q=%s&type=%s&limit=%d"
 
   def __init__(self, api_key, secret_key, user, password, auth_on_init=True):
     self.api_key = api_key
@@ -53,20 +53,20 @@ class StereoMoodClient(object):
     oauth_hook = OAuthHook(self.oauth_token, self.oauth_token_secret, self.api_key, self.secret_key, True)
     self.client = requests.session(hooks={'pre_request': oauth_hook})
 
-  def search_by_mood(self, mood):
-    return self.search_by('mood', mood)
+  def search_by_mood(self, mood, limit):
+    return self.search_by('mood', mood, limit)
 
-  def search_by_activity(self, activity):
-    return self.search_by('activity', activity)
+  def search_by_activity(self, activity, limit):
+    return self.search_by('activity', activity, limit)
 
-  def search_by_site(self, site):
-    return self.search_by('site', site)
+  def search_by_site(self, site, limit):
+    return self.search_by('site', site, limit)
 
-  def search_by(self, type, q):
+  def search_by(self, type, q, limit):
     if type not in ('mood', 'activity', 'site'):
       return None
 
-    response = self.client.get(self.SEARCH_URL % (quote_plus(q), type))
+    response = self.client.get(self.SEARCH_URL % (quote_plus(q), type, limit))
     return self.build_song_list(response.text)
 
   def download_song(self, song, save_dir):
